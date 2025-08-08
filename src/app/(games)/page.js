1e6e5e51f0bc23e3048b91cbc83869ecc4df4728
swapnilab <swapnilab9@gmail.com>
2025-08-08 11:49:44 +0530

    logout button added

--- src/app/(games)/page.js
+++ src/app/(games)/page.js
@@ -50,26 +50,26 @@ const Welcome = () => {
 
   // Create a new room
   const handleCreateRoom = async () => {
-    if (!playerName) return alert("Enter your name");
+    //if (!playerName) return alert("Enter your name");
 
     const roomsRef = ref(database, "rooms");
     const newRoomRef = push(roomsRef); // unique ID
 
     await set(newRoomRef, {
       players: {
-        host: { name: playerName },
+        host: { name: "playerName" },
       },
       gameState: "waiting",
     });
 
     setRoomId(newRoomRef.key);
-    setJoinedRoomData({ roomId: newRoomRef.key, host: playerName });
+    setJoinedRoomData({ roomId: newRoomRef.key, host: "playerName" });
     setError("");
   };
 
   // Join an existing room
   const handleJoinRoom = async () => {
-    if (!playerName || !roomId) return alert("Enter your name and room ID");
+    if ( !roomId) return alert("Enter your name and room ID");
 
     const roomRef = ref(database, `rooms/${roomId}`);
     const snapshot = await get(roomRef);
@@ -77,9 +77,9 @@ const Welcome = () => {
     if (snapshot.exists()) {
       const timestamp = Date.now().toString();
       await update(roomRef, {
-        [`players/${timestamp}`]: { name: playerName },
+        [`players/${timestamp}`]: { name: "playerName" },
       });
-      setJoinedRoomData({ roomId, player: playerName });
+      setJoinedRoomData({ roomId, player: "playerName" });
       setError("");
     } else {
       setError("Room not found");
@@ -87,6 +87,18 @@ const Welcome = () => {
   };
 
   console.log("check user details", userDetails);
+
+
+  async function handleLogout() {
+    try {
+      await auth.signOut()
+      router.push("/login");
+    }
+    catch{
+      console.log("failed to logout")
+    }
+    
+  }
 
   if (userDetails.isFetching)
     return (
@@ -103,16 +115,11 @@ const Welcome = () => {
           I already have a room id?
           <br />
           Create Room And Play
+          <button onClick={handleLogout}>Logout</button>
           <div style={{ padding: 20 }}>
             <h1> Game Room Lobby</h1>
 
-            <input
-              type="text"
-              placeholder="Your Name"
-              value={playerName}
-              onChange={(e) => setPlayerName(e.target.value)}
-              style={{ marginBottom: 10, display: "block" }}
-            />
+            
 
             <button onClick={handleCreateRoom}>Create Room</button>
 
